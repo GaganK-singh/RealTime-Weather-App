5360eddd4c6da8cd3c1886f60aa458121136e23d
Gagan Kumar Singh <41460460+GaganK-singh@users.noreply.github.com>
2023-12-14 16:53:41 +0530

    script.js

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 async function fetchCityAndWeather() {
     try {
-        const { latitude , longitude } = await fetchCity();
-        await fetchWeather(latitude, longitude);
+        const { lat , lon} = await fetchCity();
+        await fetchWeather(lat, lon);
     } catch (error) {
         console.error('Error fetching data:', error);
     }
 }
 
 async function fetchCity() {
-    const cityApiKey = '57b4d3453841434d8d2dc11c482bfbdd';
+    const cityapi = "57b4d3453841434d8d2dc11c482bfbdd"
     return new Promise((resolve, reject) => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(async position => {
                 const { latitude, longitude } = position.coords;
-
-                const reverseGeocodeApiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${cityApiKey}`;
+                const reverseGeocodeApiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${cityapi}`;
 
                 try {
                     const reverseGeocodeResponse = await fetch(reverseGeocodeApiUrl);
                     const reverseGeocodeData = await reverseGeocodeResponse.json();
-                    const cityName = reverseGeocodeData.results[0].components.city;
 
-                    document.getElementById('city').textContent = cityName;
+                    document.getElementById('city').textContent = reverseGeocodeData.results[0].components.city;
                     resolve({ lat: latitude, lon: longitude });
                 } catch (error) {
                     reject(error);
@@ -34,7 +32,7 @@ async function fetchCity() {
 }
 
 async function fetchWeather(lat, lon) {
-    const weatherApiKey = 'YOUR_API_KEY_HERE';
+    const weatherApiKey = '421ea61fa6e64ae138d1a87b81471882';
     const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
 
     try {
